ba00064f47d2a95a90b489a7d61c157bdc9253ce
pelipenko <pdashug@gmail.com>
2016-10-09 21:54:28 +0300

    Update MusicLibrary.pm

--- homeworks/music_library/lib/Local/MusicLibrary.pm
+++ homeworks/music_library/lib/Local/MusicLibrary.pm
@@ -21,4 +21,43 @@ our $VERSION = '1.00';
 
 =cut
 
+use Local::Modules::Filter qw(filter);
+use Local::Modules::Printer qw(printer);
+
+use Exporter 'import';
+our @EXPORT_OK = ('table');
+
+sub table {
+    my ( $arr, $opt ) = @_;
+    my @table;
+
+    #определение массива с шириной колонок
+    my %width;
+    for ( keys %{ ${$arr}[0] } ) { $width{$_} = 0 }
+
+    #составление таблицы
+    filter( $arr, \@table, $opt, \%width );
+
+    #сортировка
+    if ( $$opt{sort} ) {
+        if ( $$opt{sort} ne "year" ) {
+            @table =
+              sort { $$a{ $$opt{sort} } cmp $$b{ $$opt{sort} } } @table;
+        }
+        else {
+            @table =
+              sort { $$a{ $$opt{sort} } <=> $$b{ $$opt{sort} } } @table;
+        }
+    }
+
+    #вывод на экран
+    if ( $$opt{columns} ) {
+        unless ( @{ ${$opt}{columns} } ) { return }
+        printer( \@table, \%width, $$opt{columns} );
+    }
+    else {
+        printer( \@table, \%width,
+            [ "band", "year", "album", "track", "format" ] );
+    }
+}
 1;
