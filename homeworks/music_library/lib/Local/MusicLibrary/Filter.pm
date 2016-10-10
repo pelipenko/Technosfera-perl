352ff36a7661fbd3dff1c91eea391b1d89c9b8f5
pelipenko <pdashug@gmail.com>
2016-10-10 18:48:10 +0300

    Add files via upload

---
+++ homeworks/music_library/lib/Local/MusicLibrary/Filter.pm
@@ -0,0 +1,35 @@
+package Local::Modules::Filter;
+
+use strict;
+use warnings;
+
+use Exporter 'import';
+our @EXPORT_OK = ('filter');
+
+sub filter {
+    my ( $arr, $table, $opt, $width ) = @_;
+    for my $n (@$arr) {
+
+        my $flag = "";
+        for ( keys %$n ) {
+            if ( $$opt{$_}
+                and ( $$opt{$_} ne $$n{$_} and $_ ne "year" )
+                || ( $$opt{$_} != $$n{$_} and $_ eq "year" ) )
+            {
+                $flag++;
+                last;
+            }
+        }
+
+        if ($flag) { next }
+
+        push @$table, $n;
+        for ( keys %$n ) {
+            if ( length $$n{$_} > ${$width}{$_} ) {
+                ${$width}{$_} = length $$n{$_};
+            }
+        }
+
+    }
+}
+1;
